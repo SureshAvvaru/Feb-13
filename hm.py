6bfffb52de7a861d95dc869dc5547b7ff0a5aabc
sureshavvaru <suresh.avvaru123@gmail.com>
2018-02-13 13:44:48 +0530

    python code

---
+++ hm.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+Organisation = "H&M,USA"
+Products = "Mens,Womens & Childrens Clothing Market"
+Address = "Address: Madhapur,Hyderabad,Pincode:500032"
+Timings = "Visiting Timings: 11:00 AM TO 10 PM"
+WebSite = "For OnlineShoping Visit:http://www.hm.com/in"
+Mobileno = "ContactNumber:8099496986"
+
+print(Organisation)
+print(Products)
+print(Address)
+print(Timings)
+print(WebSite)
+print(Mobileno)
+
