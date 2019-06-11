853bcebe8fc087936ef3df4faeb9e6aa0c22d14f
sureshavvaru <suresh.avvaru123@gmail.com>
2019-06-11 07:43:38 +0000

    updating the students list

---
+++ file1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+student1 = 10
+student2 = 15
+student3 = 18
+
+students1 = student1 + student2
+students2 = student1 + student3
+students3 = student2 + student3
+
+print(students1)
+print(students2)
+print(students3) 
